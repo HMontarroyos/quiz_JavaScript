93a01838f689b6539e1e373aaa99b3a33a2cbe0f
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2021-01-25 15:34:13 -0300

    Primeiro Dia - Criando Projeto Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Footer from '../src/Components/Footer';
+import GitHubCorner from '../src/Components/GitHubCorner';
+import QuizBackground from '../src/Components/QuizBackground';
+import QuizLogo from '../src/Components/QuizLogo';
+import Widget from '../src/Components/Widget';
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Demais Quizes de Front-end da Comunidade</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Desbrave a Galáxia e Adquira mais conhecimentos com os Quizes das Principais Tecnologias de Front-end</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/HMontarroyos" />
+    </QuizBackground>
+  );
 }

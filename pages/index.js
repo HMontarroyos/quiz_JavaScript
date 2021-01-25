ed68756426cf365379fcdec3cba98b5fd09eb187
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2021-01-25 16:01:29 -0300

    Adicionando Tags Meta

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Head from 'next/head'
 import db from '../db.json';
 
 import Footer from '../src/Components/Footer';
@@ -21,28 +22,37 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
-
-        <Widget>
-          <Widget.Header>
-            <h1>Demais Quizes de Front-end da Comunidade</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>Desbrave a Galáxia e Adquira mais conhecimentos com os Quizes das Principais Tecnologias de Front-end</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/HMontarroyos" />
+      <div>
+        <Head>
+          <title>Quiz Mestre Jedi em JavaScript</title>
+          <meta property="og:title" content="My page title" key="title" />
+          <meta property="og:image" content={db.bg}/>
+          <meta property="og:image:type" content={db.bg}/>
+          <meta property="og:image:width" content="800"/>
+          <meta property="og:image:height" content="600"/> 
+        </Head>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Header>
+              <h1>Demais Quizes de Front-end da Comunidade</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>Desbrave a Galáxia e Adquira mais conhecimentos com os Quizes das Principais Tecnologias de Front-end</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/HMontarroyos" />
+      </div>
     </QuizBackground>
   );
 }
